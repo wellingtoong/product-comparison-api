4e918fb7db827a096a70c12b852b249097140a55
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2025-08-17 14:16:24 -0300

    feat(products): add initial controller with placeholder methods

---
+++ src/controllers/productController.js
@@ -0,0 +1,5 @@
+export const getAllProducts = async (req, res, next) => {};
+
+export const getProductById = async (req, res, next) => {};
+
+export const getProductsForComparison = async (req, res, next) => {};
